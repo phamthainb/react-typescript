9d7ccdf1005f52028d6d2a5a326922452fa200d1
phamthainb <phamthai4643@gmail.com>
2020-08-24 18:02:35 +0700

    set .env

--- server/port.js
+++ server/port.js
@@ -1,3 +1,9 @@
 const argv = require('./argv');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: path.resolve(`.env`),
+});
 
 module.exports = parseInt(argv.port || process.env.PORT || '3000', 10);
